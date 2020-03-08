2248f3423d8bb9cfc9ade7681fb42ca554803048
Raymond Wang <raymondwang@u.northwestern.edu>
2020-03-07 18:07:28 -0800

    minor

--- main.py
+++ main.py
@@ -78,7 +78,7 @@ if args.task == 'regression':
 else:
     best_mae_error = 0.
 
-print('cuda: {}'.format(args.cuda))
+print('cuda: {}'.format(args.cuda), flush=True)
 
 def main():
     global args, best_mae_error
@@ -268,7 +268,7 @@ def train(train_loader, model, criterion, optimizer, epoch, normalizer):
                       'MAE {mae_errors.val:.3f} ({mae_errors.avg:.3f})'.format(
                        epoch, i, len(train_loader), batch_time=batch_time,
                        data_time=data_time, loss=losses, mae_errors=mae_errors)
-                      )
+                      , flush=True)
             else:
                 print('Epoch: [{0}][{1}/{2}]\t'
                       'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
@@ -283,7 +283,7 @@ def train(train_loader, model, criterion, optimizer, epoch, normalizer):
                        data_time=data_time, loss=losses, accu=accuracies,
                        prec=precisions, recall=recalls, f1=fscores,
                        auc=auc_scores)
-                      )
+                      , flush=True)
 
 
 def validate(val_loader, model, criterion, normalizer, test=False):
@@ -370,7 +370,7 @@ def validate(val_loader, model, criterion, normalizer, test=False):
                       'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
                       'MAE {mae_errors.val:.3f} ({mae_errors.avg:.3f})'.format(
                        i, len(val_loader), batch_time=batch_time, loss=losses,
-                       mae_errors=mae_errors))
+                       mae_errors=mae_errors), flush=True)
             else:
                 print('Test: [{0}/{1}]\t'
                       'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
@@ -382,7 +382,7 @@ def validate(val_loader, model, criterion, normalizer, test=False):
                       'AUC {auc.val:.3f} ({auc.avg:.3f})'.format(
                        i, len(val_loader), batch_time=batch_time, loss=losses,
                        accu=accuracies, prec=precisions, recall=recalls,
-                       f1=fscores, auc=auc_scores))
+                       f1=fscores, auc=auc_scores), flush=True)
 
     if test:
         star_label = '**'
@@ -396,11 +396,11 @@ def validate(val_loader, model, criterion, normalizer, test=False):
         star_label = '*'
     if args.task == 'regression':
         print(' {star} MAE {mae_errors.avg:.3f}'.format(star=star_label,
-                                                        mae_errors=mae_errors))
+                                                        mae_errors=mae_errors), flush=True)
         return mae_errors.avg
     else:
         print(' {star} AUC {auc.avg:.3f}'.format(star=star_label,
-                                                 auc=auc_scores))
+                                                 auc=auc_scores), flush=True)
         return auc_scores.avg
 
 
